9d06e73a58a50d79a002b5b99584c28597554fb0
Joe756m <Joseph756m@hotmail.com>
2018-09-23 00:00:45 +0100

    Game Uploaded

---
+++ Game/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void FixedUpdate()
+    {
+        var z = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var x = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(x, 0, z);
+    }
+}
