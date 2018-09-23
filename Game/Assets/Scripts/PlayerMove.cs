4e6345959506d8fe58e4a41c0208a91eab84ea2b
Joe756m <Joseph756m@hotmail.com>
2018-09-23 19:30:58 +0100

    Character ontroller is done

--- Game/Assets/Scripts/PlayerMove.cs
+++ Game/Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,7 @@ public class PlayerMove : MonoBehaviour {
     [SerializeField] private float movementSpeed, jumpMultiplier;
     [SerializeField] private AnimationCurve jumpFalloff;
     [SerializeField] private KeyCode jumpKey;
+    [SerializeField] private float slopeForce, slopeForceRayLengthy;
 
     private CharacterController charController;
 
@@ -32,13 +33,16 @@ public class PlayerMove : MonoBehaviour {
 
     private void PlayerMovement(){
 
-        float horInput = Input.GetAxis(horizontalInputName) * movementSpeed;
-        float verInput = Input.GetAxis(verticalInputName) * movementSpeed;
+        float horInput = Input.GetAxis(horizontalInputName);
+        float verInput = Input.GetAxis(verticalInputName);
 
         Vector3 forwardMovement = transform.forward * verInput;
         Vector3 rightMovement = transform.right * horInput;
 
-        charController.SimpleMove(forwardMovement + rightMovement);
+        charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        if ((verInput != 0 || horInput != 0) && OnSlope())
+            charController.Move(Vector3.down * charController.height / 2 * slopeForce * Time.deltaTime);
 
         JumpInput();
     }
@@ -66,6 +70,19 @@ public class PlayerMove : MonoBehaviour {
 
         charController.slopeLimit = 45.0f;
         isJuming = false;
+    }
 
+    private bool OnSlope(){
+
+        if (isJuming)
+            return false;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * slopeForceRayLengthy))
+            if (hit.normal != Vector3.up)
+                return true;
+
+        return false;
     }
 }
