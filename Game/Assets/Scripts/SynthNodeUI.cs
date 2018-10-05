d0fed42b1c0559f95c9ed01b0256b5866132b2ba
Joe756m <Joseph756m@hotmail.com>
2018-10-05 22:09:59 +0100

    0.1 of Synth Node

---
+++ Game/Assets/Scripts/SynthNodeUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SynthNodeUI : MonoBehaviour {
+
+    [SerializeField] private Slider frequency;
+    [SerializeField] private Text currtFrequency;
+
+    [SerializeField] private Slider speed;
+    [SerializeField] private Text currtSpeed;
+
+    [SerializeField] private Slider volume;
+    [SerializeField] private Text currtVolume;
+
+    [SerializeField] private Button complete;
+
+
+    // Use this for initialization
+    void Start () {
+        complete.onClick.AddListener(completeClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currtFrequency.text = frequency.value.ToString();
+        currtSpeed.text = speed.value.ToString();
+        currtVolume.text = volume.value.ToString();
+    }
+
+    void completeClick()
+    {
+        gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public float getFrequency()
+    {
+        return frequency.value;
+    }
+
+    public float getSpeed()
+    {
+        return speed.value;
+    }
+
+    public float getVolume()
+    {
+        return volume.value;
+    }
+}
