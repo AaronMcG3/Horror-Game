ac64ad16eb69a80a289d0363210f872771559416
Joe756m <Joseph756m@hotmail.com>
2018-10-05 23:18:25 +0100

    0.2 Synth Node

---
+++ Game/Assets/Scripts/SynthNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynthNode : MonoBehaviour {
+
+    [SerializeField] private GameObject synthNodeUI;
+
+    private void OnMouseDown()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        synthNodeUI.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
